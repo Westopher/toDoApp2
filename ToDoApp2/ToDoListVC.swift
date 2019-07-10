a3992c81ca9b7b1bdffea9b8f743ad62b648e0ad
West Kraemer <kraemerwest@gmail.com>
2019-07-10 10:14:39 +0200

    add tableview funcs to get array to populate table view

--- ToDoApp2/ToDoListVC.swift
+++ ToDoApp2/ToDoListVC.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ToDoListVC: UITableViewController {
 
+    let itemArray = ["Run", "Breakfast", "Code"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
     }
 
 
