e0574265d1410741819987baacb7ae0a0e4acd32
West Kraemer <kraemerwest@gmail.com>
2019-07-10 09:12:09 +0200

    to do app 2 with coredata and realm

---
+++ ToDoApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp2
+//
+//  Created by West Kraemer on 7/10/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
